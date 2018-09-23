325a9c9e4f2e2cba4e275ef11360c7dacc861b03
Tr4vi5 <trav.dunn@outlook.com>
2018-09-23 12:15:36 -0500

    local state added to map component

--- src/components/Discover/MapContainer/MapContainer.js
+++ src/components/Discover/MapContainer/MapContainer.js
@@ -2,25 +2,28 @@ import React from 'react';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 export class MapContainer extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+        }
+    }
     onMarkerClick = () => {
         console.log('Hello');
     }
 
     render() {
         return (
-            <Map google={this.props.google} initialCenter={{ lat: 44.9782629, lng: - 93.2633184 }} zoom={14}>
+            <Map 
+                google={this.props.google} 
+                initialCenter={{ lat: 44.9782629, lng: - 93.2633184 }} 
+                zoom={14}
+            >
 
                 <Marker onClick={this.onMarkerClick}
                     name={'Current location'} />
 
                 <Marker onClick={this.onMarkerClick}
                     name={'Current location'} />
-
-                <InfoWindow onClose={this.onInfoWindowClose}>
-                    <div>
-                        <h1>Hi</h1>
-                    </div>
-                </InfoWindow>
             </Map>
         );
     }
